2346229ff735b77ea1216193c2c1301afdb5a347
Miller Adulu <milleradulu@gmail.com>
2019-04-12 01:21:01 +0300

    Initial commit

---
+++ src/utils/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAX93ua_7PjfOlKcjgv3oTksxmakxFT-tU",
+  authDomain: "patientsoileralert.firebaseapp.com",
+  databaseURL: "https://patientsoileralert.firebaseio.com",
+  projectId: "patientsoileralert",
+  storageBucket: "patientsoileralert.appspot.com",
+  messagingSenderId: "195758614444"
+};
+firebase.initializeApp(config);

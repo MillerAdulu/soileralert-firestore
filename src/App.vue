8cb34d93cfc33431fb2111cd5c0a998c032846a4
Miller Adulu <milleradulu@gmail.com>
2019-04-13 02:59:20 +0300

    Beautify the UI

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,54 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>SoilerAlert</span>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      fixed
+      app
+    >
+      <v-list dense>
+        <template v-for="item in items">
+          <v-layout v-if="item.heading" :key="item.heading" row align-center>
+            <v-flex xs6>
+              <v-subheader v-if="item.heading">{{ item.heading }}</v-subheader>
+            </v-flex>
+            <v-flex xs6 class="text-xs-center">
+              <a href="#!" class="body-2 black--text">EDIT</a>
+            </v-flex>
+          </v-layout>
+          <v-list-tile v-else :key="item.text" @click="route(item.name)">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.text }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      color="blue darken-3"
+      dark
+      app
+      fixed
+    >
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+        <v-toolbar-side-icon
+          @click.stop="drawer = !drawer"
+        ></v-toolbar-side-icon>
+        <span class="hidden-sm-and-down">Soiler Alerts</span>
       </v-toolbar-title>
     </v-toolbar>
+
     <v-content>
-      <router-view></router-view>
+      <v-container fluid fill-height>
+        <v-layout justify-center>
+          <router-view></router-view>
+        </v-layout>
+      </v-container>
     </v-content>
   </v-app>
 </template>
@@ -15,7 +57,18 @@
 export default {
   name: "App",
   data() {
-    return {};
+    return {
+      drawer: null,
+      items: [
+        { icon: "add_alert", text: "Alerts", name: "home" },
+        { icon: "settings", text: "Sensors", name: "sensors" }
+      ]
+    };
+  },
+  methods: {
+    route(name) {
+      this.$router.push({ name });
+    }
   }
 };
 </script>

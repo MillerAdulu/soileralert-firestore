2148249188c446ba5208737fc43bcf148dbf4397
Miller Adulu <milleradulu@gmail.com>
2019-04-13 02:31:29 +0300

    View sensor data

---
+++ src/views/Sensors.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <v-toolbar flat color="white">
+      <v-toolbar-title>Sensors</v-toolbar-title>
+      <v-divider class="mx-2" inset vertical></v-divider>
+    </v-toolbar>
+    <v-data-table :headers="headers" :items="sensors" class="elevation-1">
+      <template v-slot:items="props">
+        <td>{{ props.item['.key'] }}</td>
+        <td>{{ props.item.bedNo }}</td>
+        <td>{{ props.item.createdAt.toDate() }}</td>
+      </template>
+    </v-data-table>
+  </div>
+</template>
+
+<script>
+import { db } from "@/plugins/firebase";
+
+export default {
+  name: "Sensors",
+  data() {
+    return {
+      sensors: [],
+      headers: [
+        { text: "Sensor", value: ".key" },
+        { text: "Bed", value: "bed" },
+        { text: "Added At", value: "createdAt" },
+      ]
+    };
+  },
+  firestore() {
+    return {
+      sensors: {
+        ref: db.collection("sensors")
+      }
+    };
+  },
+};
+</script>

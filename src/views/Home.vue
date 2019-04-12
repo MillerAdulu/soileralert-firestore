5a81b5d21ea646cb97a4ab45f48153301f0477b1
Miller Adulu <milleradulu@gmail.com>
2019-04-13 02:22:36 +0300

    Allow nurses to mark alerts as handled in real-time

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
-    <div v-for="(alert, id) in alerts" :key="id">
-      <p>Humidity: {{ alert.humidity }}</p>
-      <p>Temp: {{ alert.temp }}</p>
-      <p>Time: {{ alert.time.toDate() }}</p>
-      <!-- <p>Sensor: {{ alert.sensorId }}</p> -->
-    </div>
+    <v-toolbar flat color="white">
+      <v-toolbar-title>Soiler Alerts</v-toolbar-title>
+      <v-divider class="mx-2" inset vertical></v-divider>
+    </v-toolbar>
+    <v-data-table :headers="headers" :items="alerts" class="elevation-1">
+      <template v-slot:items="props">
+        <td>{{ props.item.bed }}</td>
+        <td class="text-xs-right">{{ props.item.humidity }}</td>
+        <td class="text-xs-right">{{ props.item.temp }}</td>
+        <td class="text-xs-right">{{ props.item.time.toDate() }}</td>
+        <td class="text-xs-right">
+          <v-btn @click="handled(props.item)" color="green" class="white--text"
+            >Handled!</v-btn
+          >
+        </td>
+      </template>
+    </v-data-table>
   </div>
 </template>
 
@@ -16,27 +27,31 @@ export default {
   name: "Home",
   data() {
     return {
-      alerts: []
+      alerts: [],
+      headers: [
+        { text: "Bed", value: "bed" },
+        { text: "Humidity", value: "humidity" },
+        { text: "Temperature", value: "temp" },
+        { text: "Time", value: "time" },
+        { text: "Actons", value: "action" }
+      ]
     };
   },
-  // methods: {
   firestore() {
     return {
       alerts: {
-        ref: db.collection("alerts").orderBy("time"),
-        // objects: true,
-        resolve: data => {
-          console.log(data);
-        },
-        reject: err => {
-          console.log(err);
-        }
+        ref: db.collection("alerts").where("handled", "==", false)
       }
     };
+  },
+  methods: {
+    handled(alert) {
+      db.collection("alerts")
+        .doc(alert[".key"])
+        .update({
+          handled: true
+        });
+    }
   }
-  // },
-  // created() {
-  //   this.firestore();
-  // }
 };
 </script>
